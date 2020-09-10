03101896d521fb9e73fd3b414dc912fb59b9de5b
Donny Riantori <odonplay@gmail.com>
2020-09-10 18:20:02 +0700

    fix the error
    roll back to the first commit

--- js/walking_line_tab.js
+++ js/walking_line_tab.js
@@ -2,110 +2,79 @@
  * Author: Donny R
  * Repo: https://github.com/odonplay/walking-line-tab
  */
-(function( $ ) {
-  $.fn.tabLine = function(options) {
+(function ($) {
+  $.fn.tabLine = function (options) {
+    var params = $.extend(
+      {
+        speed: 500,
+        easing: "linear",
+      },
+      options
+    );
 
-  var plugin = $(this);
-  var params = $.extend({
-    speed: 500,
-    easing: "linear",
-  }, options);
-     
-  var tabContent = ".wlt__content";
-  var $tabContentFirst = $(tabContent + ":first");
-  var $navTarget = plugin.find(".wlt__nav_wrapper li");
-  var tabActiveContent = "wlt__active_content";
-  var tabActive = 'wlt__active';
-  var line = 'wlt__line';
-  var lineEl = '<div class='+line+'></>';
-  var arrWidth  = [];
-  var sum;
-  var btnWidth;
-  var elMargin;
+    var tabContent = ".td_tab__content";
+    var $tabContentFirst = $(tabContent + ":first");
+    var $navTarget = $(".td_tab__nav_wrapper li");
+    var tabActiveContent = "td_tab__active_content";
+    var tabActive = "td_tab__active";
+    var line = '<div class="td_tab__line"></>';
+    var arrWidth = [];
+    var sum;
+    var btnWidth;
+    var elMargin;
 
-  if(plugin.find('.'+line).length === 0)
-    plugin.find('.wlt__nav_container').append(lineEl);
-
-  plugin.init = function() {
-  
     $tabContentFirst.addClass(tabActiveContent);
     $navTarget.first().addClass(tabActive);
     btnWidth = $navTarget.first().innerWidth();
-    
-    if($navTarget.first()) {
-      $('.'+line).css('width',btnWidth);
-    }
-  }
+    $(".td_tab__nav_container").append(line);
 
-  plugin.getLinePosition = function() {
-    arrWidth  = [];
+    if ($navTarget.first()) {
+      $(".td_tab__line").css("width", btnWidth);
+    }
+
     $navTarget.each(function () {
       var value = $(this).outerWidth();
       arrWidth.push(value);
     });
-  }
 
-  plugin.setTabLine = function(element) {
-    btnWidth = $(element).innerWidth();
-    var myIndex = $navTarget.index(element);
-    
-    elMargin = parseInt($(element).css('margin-right'));
-    sum = 0;
-    sum+=(elMargin * myIndex);
-    
-    for (var i = 0 ; i < myIndex; i++) {
-      sum+= arrWidth[i];
-    }
-  }
+    $navTarget.on("click", function () {
+      btnWidth = $(this).innerWidth();
+      $(this).addClass(tabActive).siblings().removeClass(tabActive);
+      var myIndex = $navTarget.index(this);
 
-  plugin.openTabContent = function(element) {
-    var prefix = $(element).attr("id");
-    $(tabContent).removeClass(tabActiveContent);
-    $('#'+ prefix + '_content').addClass(tabActiveContent);
-  }
+      elMargin = parseInt($(this).css("margin-right"));
+      sum = 0;
+      sum += elMargin * myIndex;
 
-  plugin.onClickEvent = function() {
-    $navTarget.on('click', function () {
-      var isActive = $(this).hasClass(tabActive);
-      if(!isActive) {
-        $(this).addClass(tabActive).siblings().removeClass(tabActive);
-        
-        plugin.setTabLine($(this));
-        plugin.openTabContent($(this));
-
-        $("."+line).animate({
-          left: sum + "px",
-          width: btnWidth + 'px'
-        },params.speed, params.easing);
+      for (var i = 0; i < myIndex; i++) {
+        sum += arrWidth[i];
       }
-    }); 
-  }
 
-  plugin.init();
-  plugin.getLinePosition();
-  plugin.onClickEvent();
+      var prefix = $(this).attr("id");
+      if ($(this).hasClass("td_tab__nav")) {
+        $(tabContent).removeClass(tabActiveContent);
+        $("#" + prefix + "_content").addClass(tabActiveContent);
+      }
 
-  function afterResize(){
-    plugin.getLinePosition();
-    var activeTarget = plugin.find(".wlt__nav_wrapper li." + tabActive);
-    plugin.setTabLine($(activeTarget));
-    $("."+line).css({
-      left: sum + "px",
-      width: btnWidth + 'px',
-      transition: 'all 0.1s ease-in'
+      if (myIndex) {
+        $(".td_tab__line").animate(
+          {
+            left: sum + "px",
+            width: btnWidth + "px",
+          },
+          params.speed,
+          params.easing
+        );
+      } else {
+        $(".td_tab__line").animate(
+          {
+            left: sum + "px",
+            width: btnWidth + "px",
+          },
+          params.speed,
+          params.easing
+        );
+      }
     });
-
-    setTimeout(function(e) {
-      $("."+line).css({
-        transition: ''
-      });
-    }, 200);
-  }
-
-  var resizeId;
-  $(window).on("resize", function(){
-    clearTimeout(resizeId);
-    resizeId = setTimeout(afterResize, 200);
-  });
-}
-}( jQuery ));
+  };
+})(jQuery);
